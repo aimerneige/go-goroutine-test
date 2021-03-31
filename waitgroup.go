5ccf2991adba3c89239a8c8a4205d1b2877d4d96
AimerNeige <aimer.neige.soft@gmail.com>
2021-03-31 20:54:59 +0800

    update wg

---
+++ waitgroup.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+func main() {
+	var wg sync.WaitGroup
+	wg.Add(2)
+
+	c := make(chan string)
+
+	go func() {
+		count(5, "Hello")
+		wg.Done()
+	}()
+
+	go func() {
+		go count(5, "World")
+		wg.Done()
+	}()
+
+	wg.Wait()
+}
+
+func count(n int, s string) {
+	for i := 0; i < n; i++ {
+		fmt.Println(i+1, s)
+		time.Sleep(time.Millisecond * 200)
+	}
+}
